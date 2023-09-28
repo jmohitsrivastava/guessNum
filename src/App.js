5ee79400c005debd80cc77c5b5dcbecf7c88c331
jmohitsrivastava <144433083+jmohitsrivastava@users.noreply.github.com>
2023-09-28 14:57:18 +0530

    number guesser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Button,  Container,Input} from 'reactstrap';
 
 function App() {
+ let [number,setnumber]=useState('');
+ let [randomnum,setrandomnum]=useState(Math.floor((Math.random()*100)+1));
+ let [attempt,setattempt]=useState(1);
+
+ const resetGame=()=>{
+  setnumber('');
+  setrandomnum(Math.floor((Math.random()*100)+1));
+  setattempt(1);
+
+  document.querySelector("#showresult").innerHTML=''
+ }
+ 
+ const checkNumber=()=>{
+  
+  
+  if(number===''){
+    document.querySelector("#showresult").innerHTML="<h4>Please Enter Number<h4/>"
+   }
+
+   else if(number<0 || number>100){
+      console.log("wrong"+number)
+      setnumber('');
+      document.querySelector("#showresult").innerHTML="<h4>please select number between 1 to 100<h4/>"
+    }
+    
+   else if(randomnum==number){
+
+    
+    console.log("you guess right")
+    setrandomnum(Math.floor((Math.random()*100)+1));
+    document.querySelector("#showresult").innerHTML="<h4>Congratulation:you guess right<h4/>:<br/><span>Attempt:<span/>"+attempt
+  
+  }
+
+  else if(randomnum<number){
+    
+    setattempt(attempt+1)
+
+     console.log("result is less than:"+number)
+     setnumber('');
+     document.querySelector("#showresult").innerHTML="<h4>Number is Less than: "+number;
+  }
+  
+  else if(randomnum>number){
+
+    setattempt(attempt+1)
+
+    console.log("result is greater than:"+number)
+    setnumber('');
+    document.querySelector("#showresult").innerHTML="<h4>Number is Greater than: "+number;
+  }
+    
+
+  }
+
+  const userinput=(e)=>{
+    
+    setnumber(document.querySelector(".userinp").value);
+    
+   }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Container >
+
+      
+    
+     <h1 className="text-center my-4" id='text-top'>NUMBER GUESSER GAME:1 to 100 </h1>
+
+    <div className='inside'>
+      
+     <div className="inputb" >
+
+      <div id='indiin'>
+         <Input type="number" className="userinp" id='userinput' value={number} onChange={userinput} placeholder='guess-num'></Input>
+      </div>
+         
+         <div className="submitbox">
+           <Button className="submitbtn"  onClick={checkNumber}>done</Button>
+           <Button className="submitbtn"  onClick={resetGame}>restart</Button>
+           
+       </div>
+
+        {/* for showing result */}
+        <div id='showresult' style={{marginTop:"10%",textAlign:"center",color:"black"}}>
+      
+        </div> 
+
+     </div>
+     
+     
     </div>
+    
+    
+    </Container>
+
+    
+    </>
   );
 }
 
